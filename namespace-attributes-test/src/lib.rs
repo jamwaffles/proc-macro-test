48a0ec71f9af90544f5f90d686474d21be36c04a
James Waples <jamwaffles@gmail.com>
2018-08-28 15:10:13 +0100

    Log some test JSON

--- namespace-attributes-test/src/lib.rs
+++ namespace-attributes-test/src/lib.rs
@@ -1,16 +1,26 @@
 #![feature(use_extern_macros)]
 #![feature(custom_attribute)]
 
+extern crate serde;
+extern crate serde_json;
 #[macro_use]
+extern crate serde_derive;
 extern crate namespace_attributes_macros;
 
 use namespace_attributes_macros::ns_test;
 
+#[derive(Serialize)]
 struct EventA;
+
+#[derive(Serialize)]
 struct EventB;
+
+#[derive(Serialize)]
 struct NsEventC;
 
+#[derive(Serialize)]
 #[ns_test(namespace = "test_ns")]
+#[serde(tag = "type")]
 enum Events {
     EventA(EventA),
     EventB(EventB),
@@ -20,8 +30,17 @@ enum Events {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use serde_json::to_string;
+
     #[test]
     fn it_works() {
+        let evt = Events::EventA(EventA);
+
+        let json = to_string(&evt);
+
+        println!("{:?}", json);
+
         assert_eq!(2 + 2, 4);
     }
 }
