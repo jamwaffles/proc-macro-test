19cba9506116b9efda43cfdc23d9fb8f2b865a7a
James Waples <jamwaffles@gmail.com>
2018-08-29 21:54:10 +0100

    --wip-- [skip ci]

--- namespace-attributes-macros/src/derive_struct.rs
+++ namespace-attributes-macros/src/derive_struct.rs
@@ -3,7 +3,7 @@ use proc_macro2::TokenStream;
 use quote::ToTokens;
 use syn::{DataStruct, DeriveInput};
 
-pub fn derive_struct(parsed: &DeriveInput, _body: &DataStruct) -> TokenStream {
+pub fn derive_struct(parsed: &DeriveInput, body: &DataStruct) -> TokenStream {
     let struct_namespace = get_namespace_from_attributes(&parsed.attrs)
         .expect("Namespace attribute must be provided at the struct level");
 
@@ -11,10 +11,78 @@ pub fn derive_struct(parsed: &DeriveInput, _body: &DataStruct) -> TokenStream {
 
     let namespaced_ident = format!("\"{}.{}\"", struct_namespace, item_ident);
 
+    let derive_thing = "#[derive(Deserialize, Debug)]";
+    let serde_rename = "#[serde(rename = \"type\")]";
+    let test_name = format!("{}DeserializeTest", item_ident);
+
+    let ns = format!("{}", struct_namespace);
+    let ty = format!("{}", item_ident);
+
+    let body = body.clone().fields.into_token_stream();
+
     let out = quote!{
+
+
         impl namespace_attributes_internals::EventData for #item_ident {
             fn namespaced_type(&self) -> &'static str {
                 #namespaced_ident
+            }
+        }
+
+
+
+        impl<'de> Deserialize<'de> for #item_ident {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                #[derive(Deserialize, Debug)]
+                struct Helper {
+                    #[serde(rename = "type")]
+                    pub event_type_and_namespace: Option<String>,
+                    pub event_type: Option<String>,
+                    pub event_namespace: Option<String>,
+                }
+
+                struct Output #body;
+
+                impl From<Output> for #item_ident {
+                    fn from(out: Self) -> #item_ident {
+                        #item_ident {
+                            thing: 100
+                        }
+                    }
+                }
+
+                let v = JsonValue::deserialize(deserializer).map_err(de::Error::custom)?;
+
+                let mut type_helper = Helper::deserialize(&v).map_err(de::Error::custom)?;
+
+                // Map old-style event to new-style if new-style is not defined
+                if let Some(ref ns_and_ty) = &type_helper.event_type_and_namespace {
+                    if type_helper.event_type.is_none() && type_helper.event_namespace.is_none() {
+                        let parts: Vec<String> = ns_and_ty.clone().split('.').map(|part| String::from(part)).collect();
+
+                        type_helper.event_namespace = Some(parts[0].clone());
+                        type_helper.event_type = Some(parts[1].clone());
+                    }
+                }
+
+                match (&type_helper.event_namespace, &type_helper.event_type) {
+                    (Some(ref ns), Some(ref ty)) => {
+                        // println!("MATCH {} {} / {} {} : {}, {}", ns, ty, #ns, #ty, ns == #ns, ty == #ty);
+
+                        if ns != #ns || ty != #ty {
+                            Err(de::Error::custom("Data does not match types"))
+                        } else {
+                            // println!("THING {:?}", v);
+                            let out = Output::deserialize(v);
+
+                            out.into()
+                        }
+                    },
+                    _ => Err(de::Error::custom("Could not deserialize event"))
+                }
             }
         }
     };
--- namespace-attributes-test/src/lib.rs
+++ namespace-attributes-test/src/lib.rs
@@ -3,36 +3,44 @@ extern crate serde;
 extern crate serde_json;
 extern crate namespace_attributes_internals;
 #[macro_use]
+extern crate serde_derive;
+#[macro_use]
 extern crate namespace_attributes_macros;
 
-#[derive(EventData)]
+use std::fmt;
+
+use serde::de::{self, Deserialize, Deserializer, MapAccess, SeqAccess, Visitor};
+use serde_json::{from_value, Value as JsonValue};
+use std::collections::HashMap;
+
+#[derive(EventData, Debug)]
 #[ns_test(namespace = "test_ns")]
 struct EventA {
     thing: u32,
 }
 
-#[derive(EventData)]
-#[ns_test(namespace = "test_ns")]
-struct EventB {
-    thing: u32,
-}
+// #[derive(EventData)]
+// #[ns_test(namespace = "test_ns")]
+// struct EventB {
+//     thing: u32,
+// }
 
-#[derive(EventData)]
-#[ns_test(namespace = "test_ns")]
-struct NsEventC {
-    thing: u32,
-}
+// #[derive(EventData)]
+// #[ns_test(namespace = "test_ns")]
+// struct NsEventC {
+//     thing: u32,
+// }
 
-#[derive(EventData)]
-#[ns_test(namespace = "test_ns")]
-// #[serde(tag = "type")]
-enum Events {
-    EnumEventA(EventA),
-    EnumEventB(EventB),
-    #[ns_test(namespace = "remote_ns")]
-    #[ns_test(rename = "RenamedRemoteEvent")]
-    EnumNsEventC(NsEventC),
-}
+// #[derive(EventData, Deserialize)]
+// #[ns_test(namespace = "test_ns")]
+// // #[serde(tag = "type")]
+// enum Events {
+//     EnumEventA(EventA),
+//     EnumEventB(EventB),
+//     // #[ns_test(namespace = "remote_ns")]
+//     // #[ns_test(rename = "RenamedRemoteEvent")]
+//     EnumNsEventC(NsEventC),
+// }
 
 #[cfg(test)]
 mod tests {
@@ -42,59 +50,139 @@ mod tests {
     use serde_json::to_value;
 
     #[test]
-    fn it_gets_namespaced_event_names() {
-        let event_a = Events::EnumEventA(EventA { thing: 100 });
-        let event_b = Events::EnumEventB(EventB { thing: 100 });
-        let event_c = Events::EnumNsEventC(NsEventC { thing: 100 });
-
-        assert_eq!(event_a.namespaced_type(), "test_ns.EnumEventA");
-        assert_eq!(event_b.namespaced_type(), "test_ns.EnumEventB");
-        assert_eq!(event_c.namespaced_type(), "remote_ns.EnumNsEventC");
-    }
-
-    #[test]
-    fn it_serializes_events_with_extra_fields() {
-        let event = Events::EnumEventA(EventA { thing: 100 });
-
-        let json = to_value(&event).unwrap();
-
-        assert_eq!(
-            json,
-            json!({
-                "type": "test_ns.EventA",
-                "event_namespace": "test_ns",
-                "event_type": "EventA",
-                "thing": 100,
-            })
-        );
-    }
-
-    #[test]
-    fn it_serializes_events_with_overridden_namespace() {
-        let event = Events::EnumNsEventC(NsEventC { thing: 100 });
-
-        let json = to_value(&event).unwrap();
-
-        assert_eq!(
-            json,
-            json!({
-                "type": "remote_ns.RenamedRemoteEvent",
-                "event_namespace": "remote_ns",
-                "event_type": "RenamedRemoteEvent",
-                "thing": 100,
-            })
-        );
-    }
-
-    #[test]
     fn it_deserializes_events_correctly() {
+        #[derive(EventData, Debug)]
+        #[ns_test(namespace = "remote_ns_newer")]
+        struct TestEvent {
+            thing: u32,
+        }
+
         let json = json!({
-            "type": "remote_ns.RenamedRemoteEvent",
-            "event_namespace": "remote_ns",
-            "event_type": "RenamedRemoteEvent",
+            "type": "remote_ns.TestEventOld",
+            "event_namespace": "remote_ns_newer",
+            "event_type": "TestEvent",
             "thing": 100,
         });
 
-        let event: Events = from_value(json).unwrap();
+        let event = from_value::<TestEvent>(json);
+
+        assert!(event.is_ok());
+        assert_eq!(event.unwrap().thing, 100);
     }
+
+    #[test]
+    fn it_deserializes_events_with_old_def() {
+        #[derive(EventData, Debug)]
+        #[ns_test(namespace = "remote_ns")]
+        struct TestEvent {
+            thing: u32,
+        }
+
+        let json = json!({
+            "type": "remote_ns.TestEvent",
+            "thing": 100,
+        });
+
+        let event = from_value::<TestEvent>(json);
+
+        assert!(event.is_ok());
+        assert_eq!(event.unwrap().thing, 100);
+    }
+
+    #[test]
+    fn it_deserializes_events_with_new_def() {
+        #[derive(EventData, Debug)]
+        #[ns_test(namespace = "remote_ns_newer")]
+        struct TestEvent {
+            thing: u32,
+        }
+
+        let json = json!({
+            "event_namespace": "remote_ns_newer",
+            "event_type": "TestEvent",
+            "thing": 100,
+        });
+
+        let event = from_value::<TestEvent>(json);
+
+        assert!(event.is_ok());
+        assert_eq!(event.unwrap().thing, 100);
+    }
+
+    // #[test]
+    // fn it_gets_a_namespaced_struct_type() {
+    //     #[derive(EventData)]
+    //     #[ns_test(namespace = "some_namespace")]
+    //     struct TestStruct {
+    //         thing: u32,
+    //     }
+
+    //     let thing = TestStruct { thing: 100 };
+
+    //     assert_eq!(thing.namespaced_type(), "some_namespace.TestStruct");
+    // }
+
+    // #[test]
+    // fn it_serializes_structs() {
+    //     #[derive(EventData)]
+    //     #[ns_test(namespace = "some_namespace")]
+    //     struct TestStruct {
+    //         thing: u32,
+    //     }
+
+    //     let thing = TestStruct { thing: 100 };
+
+    //     assert_eq!(
+    //         to_value(&thing).unwrap(),
+    //         json!({
+    //             "type": "some_namespace.TestStruct",
+    //             "thing": 100
+    //         })
+    //     );
+    // }
+
+    // #[test]
+    // fn it_gets_namespaced_event_names() {
+    //     let event_a = Events::EnumEventA(EventA { thing: 100 });
+    //     let event_b = Events::EnumEventB(EventB { thing: 100 });
+    //     let event_c = Events::EnumNsEventC(NsEventC { thing: 100 });
+
+    //     assert_eq!(event_a.namespaced_type(), "test_ns.EnumEventA");
+    //     assert_eq!(event_b.namespaced_type(), "test_ns.EnumEventB");
+    //     assert_eq!(event_c.namespaced_type(), "remote_ns.EnumNsEventC");
+    // }
+
+    // #[test]
+    // fn it_serializes_events_with_extra_fields() {
+    //     let event = Events::EnumEventA(EventA { thing: 100 });
+
+    //     let json = to_value(&event).unwrap();
+
+    //     assert_eq!(
+    //         json,
+    //         json!({
+    //             "type": "test_ns.EventA",
+    //             "event_namespace": "test_ns",
+    //             "event_type": "EventA",
+    //             "thing": 100,
+    //         })
+    //     );
+    // }
+
+    // #[test]
+    // fn it_serializes_events_with_overridden_namespace() {
+    //     let event = Events::EnumNsEventC(NsEventC { thing: 100 });
+
+    //     let json = to_value(&event).unwrap();
+
+    //     assert_eq!(
+    //         json,
+    //         json!({
+    //             "type": "remote_ns.RenamedRemoteEvent",
+    //             "event_namespace": "remote_ns",
+    //             "event_type": "RenamedRemoteEvent",
+    //             "thing": 100,
+    //         })
+    //     );
+    // }
 }
